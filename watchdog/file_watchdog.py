c7a32aaba2f17ecbecf9e08c96fbcb41a0d7684c
tom1299 <tom1299@jeeatwork.com>
2022-07-28 07:56:01 +0200

    Add signal handler

--- watchdog/file_watchdog.py
+++ watchdog/file_watchdog.py
@@ -1,21 +1,39 @@
 import logging
+import signal
 import sys
 import time
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 
-if __name__ == '__main__':
+LOGGER = logging.getLogger("watchdog")
+
+
+def add_signal_handler():
+    def signal_term_handler(sig, frame):
+        if observer:
+            observer.stop()
+            observer.join()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_term_handler)
+    signal.signal(signal.SIGTERM, signal_term_handler)
+
+
+if __name__ == "__main__":
+
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=False)
+    observer.schedule(LoggingEventHandler(),  path='/tmp/secrets',  recursive=False)
+
+    add_signal_handler()
+
     observer.start()
+
     try:
         while True:
             time.sleep(1)
-    finally:
-        observer.stop()
-        observer.join()
+    except BaseException:
+        pass
