ffb84afc95cef642a2a9bcf4fcaf3d65f47f9ddb
tom1299 <tom1299@jeeatwork.com>
2022-08-17 09:44:03 +0200

    Add recursion and size limit check

--- gitlab-helpers/get-group-structure.py
+++ gitlab-helpers/get-group-structure.py
@@ -1,6 +1,6 @@
+import json
 import gitlab
 import os
-from gitlab.v4.objects.groups import Group
 
 gitlab_server: gitlab.Gitlab
 
@@ -9,14 +9,26 @@ def get_gitlab_group(group_id):
     return gitlab_server.groups.get(group_id)
 
 
+def get_traces(group_id: str, size_limit_in_bytes: int):
+    group = get_gitlab_group(group_id)
+    for group_project in group.projects.list(get_all=True):
+        for job in gitlab_server.projects.get(id=group_project.id).jobs.list(get_all=True):
+            if trace_exists_and_does_not_exceed_size_limit(job, size_limit_in_bytes):
+                print(job.trace().decode("utf-8"))
+            else:
+                print("trace exceeds size limit")
+
+    for sub_group in group.subgroups.list(get_all=True):
+        get_traces(sub_group.id)
+
+
+def trace_exists_and_does_not_exceed_size_limit(job, size_limit_in_bytes):
+    for artifact in job.attributes["artifacts"]:
+        if artifact["file_type"] == "trace" and artifact["size"] <= size_limit_in_bytes:
+            return True
+    return False
+
+
 if __name__ == '__main__':
     gitlab_server = gitlab.Gitlab(url='https://gitlab.com', private_token=os.getenv("GITLAB_TOKEN"))
-    group: Group = get_gitlab_group(os.getenv("GITLAB_GROUP_ID"))
-
-    while group:
-        for group_project in group.projects.list(get_all=True):
-            project_id = group_project.id
-            project = gitlab_server.projects.get(id=project_id)
-            for job in project.jobs.list(get_all=True):
-                print(job.name)
-                print(job.trace().decode("utf-8"))
+    get_traces(os.getenv("GITLAB_GROUP_ID"), 2000)
