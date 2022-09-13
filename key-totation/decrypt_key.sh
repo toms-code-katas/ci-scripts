2817d9bce7a57049cc171508c96d6711004a0e7c
tom1299 <tom1299@jeeatwork.com>
2022-09-13 15:51:34 +0200

    Add first draft for script for age key rotation

---
+++ key-totation/decrypt_key.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/expect -f
+
+set timeout -1
+set key_password [lindex $argv 0];
+set key_file [lindex $argv 1];
+
+spawn bash -c "age -d $key_file"
+
+expect -re "Enter passphrase" {
+    send "$key_password\r"
+}
+
+expect eof
---
+++ key-totation/rotate-key.py
@@ -0,0 +1,59 @@
+import gitlab
+from io import BytesIO
+import os
+import re
+import subprocess
+import zipfile
+import tempfile
+
+
+def get_last_two_keys_from_artifacts(project, environment):
+    environment_age_keys_found = {}
+    keys_found = 0
+    for pipeline in project.pipelines.list(get_all=False):
+        for pipeline_job in pipeline.jobs.list(order_by="finished_at", scope='success', get_all=False):
+            for artifact in pipeline_job.attributes["artifacts"]:
+                try:
+                    artifact_environment = re.search("(?<=agekeys-).*(?=.zip)", artifact["filename"]).group(0)
+                    if artifact_environment == environment:
+                        print(f"Found artifact for environment {artifact_environment}")
+                        job = project.jobs.get(pipeline_job.id, lazy=True)
+                        output = BytesIO()
+                        job.artifacts(streamed=True, action=output.write)
+                        output.flush()
+                        zip = zipfile.ZipFile(output)
+                        armoured_age_key = {name: zip.read(name) for name in zip.namelist()}
+                        environment_age_keys_found.update(armoured_age_key)
+                        keys_found = keys_found + 1
+                        if keys_found == 2:
+                            print(f"Found two last keys for environment {artifact_environment}")
+                            return environment_age_keys_found
+                except AttributeError:
+                    pass
+
+
+if __name__ == '__main__':
+    # Use CI_JOB_TOKEN='[MASKED]' as private token
+    # Use CI_SERVER_URL='https://gitlab.com' as url
+    # Use CI_PROJECT_ID='9999' as project id
+    # Use ENVIRONMENT as the environment to use
+    # Use KEY_PASSWORD for the password of the two keys
+    environment = os.getenv("ENVIRONMENT")
+    gl = gitlab.Gitlab(url=os.getenv("CI_SERVER_URL"), private_token=os.getenv("CI_JOB_TOKEN"))
+
+    project = gl.projects.get(id=os.getenv("CI_PROJECT_ID"))
+
+    environment_age_keys_found = get_last_two_keys_from_artifacts(project, environment)
+    sorted_keys = sorted(environment_age_keys_found)
+
+    new_key = environment_age_keys_found[sorted_keys[0]]
+    old_key = environment_age_keys_found[sorted_keys[1]]
+
+    new_key_file = tempfile.NamedTemporaryFile(prefix="new_key-")
+    with open(new_key_file.name, "wb") as key_file:
+        key_file.write(new_key)
+    print(new_key_file.name)
+
+    subprocess.run(
+        [f"{os.path.dirname(__file__)}/decrypt_key.sh", os.getenv("KEY_PASSWORD"), new_key_file.name],
+        check=True)
