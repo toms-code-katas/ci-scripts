221920505905ce735afd0bd00724c3f5118f3149
tom1299 <tom1299@jeeatwork.com>
2022-09-29 10:24:49 +0200

    Change config file structure and switch to glob pattern

--- kyverno-analyzer/kyverno_analyzer.py
+++ kyverno-analyzer/kyverno_analyzer.py
@@ -1,3 +1,5 @@
+import glob
+import os.path
 import re
 import sys
 import yaml
@@ -19,73 +21,84 @@ class Result:
         else:
             self.errors_ignored[name] = 1
 
-    def matches_config(self, config):
+    def matches_config(self, applicable_rules):
         matches = True
-        for to_ignore in config.ignore_errors:
-            error_name = to_ignore["name"]
-            if error_name not in self.errors_ignored:
-                print(f"Expected error {error_name} not found")
+        for rule in applicable_rules:
+            rule_name = rule["name"]
+            if rule_name not in self.errors_ignored:
+                print(f"Expected match for {rule_name} not found")
                 matches = False
-            elif self.errors_ignored[error_name] != to_ignore["expected_errors"]:
-                print(f"Expected {to_ignore['expected_errors']} occurrences of error \"{error_name}\""
-                      f" found {self.errors_ignored[error_name]} occurrences")
+            elif self.errors_ignored[rule_name] != rule["expected_errors"]:
+                print(f"Expected {rule['expected_errors']} occurrences of error \"{rule_name}\""
+                      f" found {self.errors_ignored[rule_name]} occurrences")
                 matches = False
         return matches
 
 
 class Analyzer:
 
-    def __init__(self, config, report_path):
-        self.config = config
+    def __init__(self, config_path, report_path):
+        self.config = self.get_config(config_path)
         self.report_path = report_path
+        self.report_file_name = os.path.basename(report_path)
         self.result = Result()
         self.collect = False
         self.current_message = ""
+        self.applicable_rules = []
+        self.select_rules_for_report_file()
 
     def analyze(self):
         with open(self.report_path, 'r') as handle:
             for event in yaml.parse(handle):
                 if isinstance(event, yaml.MappingStartEvent):
                     self.collect = True
-                elif isinstance(event, yaml.MappingEndEvent):
+                # only if rule has been added to the current message is the message complete
+                elif isinstance(event, yaml.MappingEndEvent) and "rule" in self.current_message:
                     self.analyze_current_message()
                     self.current_message = ""
                 elif self.collect:
                     if hasattr(event, "value"):
                         self.current_message = self.current_message + event.value + " "
 
-        if not self.result.matches_config(self.config):
+        if not self.result.matches_config(self.applicable_rules):
             print(f"Expected errors do not match found errors")
             return False
+        return True
 
     def analyze_current_message(self):
         self.collect = False
-        for error_to_ignore in self.config.ignore_errors:
+        for rule in self.applicable_rules:
             all_matches_found = True
-            for pattern in error_to_ignore["patterns"]:
+            for pattern in rule["patterns"]:
                 if not re.search(pattern, self.current_message):
                     all_matches_found = False
                     break
+
             if all_matches_found:
-                print(f"message: \"{self.current_message}\" matches ignore pattern \"{error_to_ignore['name']}\"")
-                self.result.add_ignored_error(error_to_ignore["name"])
-                return
+                print(f"message: \"{self.current_message}\" matches ignore pattern \"{rule['name']}\"")
+                self.result.add_ignored_error(rule["name"])
+            else:
+                return False
 
+    def get_config(self, config_path):
+        with open(config_path) as f:
+            config_map = yaml.safe_load(f)
+            return Config(**config_map)
 
-def get_config(path_to_config_file):
-    with open(path_to_config_file) as f:
-        # use safe_load instead load
-        config_map = yaml.safe_load(f)
-        return Config(**config_map)
+    def select_rules_for_report_file(self):
+        for rule in self.config.rules:
+            if self.report_file_name in rule["files"]:
+                self.applicable_rules.append(rule)
 
 
 # To remove the first lines of the report which are not yaml use:
 # tail -n +7 policy-report.yaml > policy-report-without-header.yaml
 if __name__ == '__main__':
 
-    cfg = get_config(sys.argv[2])
-
-    analyzer = Analyzer(cfg, sys.argv[1])
-    if not analyzer.analyze():
-        exit(1)
-    exit(0)
+    error_found = False
+    for report in glob.glob(sys.argv[1], recursive=True):
+        print(f"Analyzing file {report}")
+        analyzer = Analyzer(sys.argv[2], report)
+        if not analyzer.analyze():
+            error_found = True
+    exit(1 if error_found else 0)
--- kyverno-analyzer/kyverno_analyzer_test.py
+++ kyverno-analyzer/kyverno_analyzer_test.py
@@ -14,7 +14,7 @@ class TestAnalyzer(unittest.TestCase):
 
     def test_matches(self):
         path = os.path.dirname(__file__)
-        config = path + "/config/config-1.yaml"
+        config = path + "/config/config-2.yaml"
         report_path = path + "/report/test-2-policy-report.yaml"
         analyzer = Analyzer(config, report_path)
         assert analyzer.analyze()
