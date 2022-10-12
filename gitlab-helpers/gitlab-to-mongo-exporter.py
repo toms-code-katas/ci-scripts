dea0d4b6bc76cc49ae4766bbef52289c6b8bc9cd
tom1299 <tom1299@jeeatwork.com>
2022-10-12 15:21:33 +0200

    Finished latest date retrieval for sinks

--- gitlab-helpers/gitlab-to-mongo-exporter.py
+++ gitlab-helpers/gitlab-to-mongo-exporter.py
@@ -13,6 +13,11 @@ from pymongo import MongoClient
 from typing import Dict
 
 
+def get_time(latest, offset: int = 0):
+    latest = latest.replace("Z", "+00:00")
+    return datetime.datetime.fromisoformat(latest) + datetime.timedelta(seconds=offset)
+
+
 class Sink(ABC):
 
     @abc.abstractmethod
@@ -21,6 +26,10 @@ class Sink(ABC):
 
     @abc.abstractmethod
     def already_added(self, document: Dict, doc_type: str) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def date_of_latest_document(self, doc_type: str, date_field_name: str = "finished_at"):
         pass
 
 
@@ -36,8 +45,25 @@ class MongoDbSink(Sink):
     def already_added(self, document: Dict, doc_type: str) -> bool:
         return self.mongo_db["jobs"].find_one({"id": document["id"]}) is not None
 
+    def date_of_latest_document(self, doc_type: str, date_field_name: str = "finished_at"):
+        # db.jobs.find({ "finished_at": { $ne: null } }).sort({finished_at:-1}).limit(1)
+        latest = self.mongo_db["jobs"].find(filter={date_field_name: {"$ne": "null"}},
+                                            sort=[(date_field_name, -1)], limit=1,
+                                            projection=[date_field_name]).next()
+        if latest:
+            return get_time(latest[date_field_name])
+        return None
+
 
 class ElasticSink(Sink):
+
+    def date_of_latest_document(self, doc_type: str, date_field_name: str = "finished_at"):
+        result = self.es_client.search(index=doc_type, query={"match_all": {}},
+                                       sort=[{date_field_name: {"order": "desc"}}], size=1)
+        hits = result.body["hits"]["hits"]
+        if hits:
+            return get_time(hits[0]["_source"][date_field_name])
+        return None
 
     def __init__(self, host: str = "localhost", port: int = 9200):
         self.es_client = Elasticsearch(f"http://{host}:{port}")
@@ -84,27 +110,30 @@ class ExportRepository:
 # https://refactoring.guru/design-patterns/visitor
 class GetJobsAndTraces:
 
-    def __init__(self, latest_job_per_project: {}, trace_size_limit: int, sinks: [Sink]):
-        self.latest_job_per_project = latest_job_per_project
+    def __init__(self, trace_size_limit: int, sinks: [Sink]):
         self.trace_size_limit = trace_size_limit
         self.sinks = sinks
+        self.finished_after = datetime.datetime.now(tz=datetime.timezone.utc) - datetime.timedelta(
+            weeks=1)
+        self.determine_latest_job_date()
 
     def process(self, project):
-        if project.id in self.latest_job_per_project:
-            updated_after = self.latest_job_per_project[project.id]
-        else:
-            updated_after = datetime.datetime.now(tz=datetime.timezone.utc) - datetime.timedelta(
-                days=365)
-
         new_jobs = 0
-        for pipeline in project.pipelines.list(updated_after=updated_after, get_all=True):
+        for pipeline in project.pipelines.list(updated_after=self.finished_after, get_all=True):
             for pipeline_job in pipeline.jobs.list(get_all=True):
-                if get_time(pipeline_job.created_at) > updated_after:
+                if pipeline_job.finished_at and get_time(
+                        pipeline_job.finished_at) > self.finished_after:
                     job = project.jobs.get(pipeline_job.id)
                     self.output_job_and_trace(job)
                     new_jobs += 1
 
         print(f"Added {new_jobs} new jobs for project \"{project.name}\"")
+
+    def determine_latest_job_date(self):
+        for sink in self.sinks:
+            latest_date_from_sink = sink.date_of_latest_document("jobs", "finished_at")
+            if latest_date_from_sink and latest_date_from_sink > self.finished_after:
+                self.finished_after = latest_date_from_sink
 
     def output_job_and_trace(self, job):
         job_as_dict = job.asdict()
@@ -145,11 +174,6 @@ def traverse_all_projects_in_group(group_id: str, processors):
         traverse_all_projects_in_group(sub_group.id, processors)
 
 
-def get_time(latest, offset: int = 0):
-    latest = latest.replace("Z", "+00:00")
-    return datetime.datetime.fromisoformat(latest) + datetime.timedelta(seconds=offset)
-
-
 def get_latest_job_date_per_project(mongo_db):
     # db.jobs.aggregate([ { $group: { _id: "$pipeline.project_id", latests: { $max: "$pipeline.updated_at" } } }])
     result = {}
@@ -168,17 +192,15 @@ def get_latest_job_date_per_project(mongo_db):
 if __name__ == '__main__':
     mongo_client = MongoClient('mongodb://localhost:27017/')
     mongo_db = mongo_client['gitlab']
-    latest_job_per_project = get_latest_job_date_per_project(mongo_db)
-    latest_job_per_project = {}
+    # latest_job_per_project = get_latest_job_date_per_project(mongo_db)
 
     stdout_sink = type('StdOutSink', (Sink, object), {"sink": lambda document, doc_type: print(
         json.dumps(document, indent=2)),
                                                       "already_added": lambda document, doc_type:
                                                       False})
 
-    get_jobs_and_traces = GetJobsAndTraces(latest_job_per_project=latest_job_per_project,
-                                           trace_size_limit=10000,
-                                           sinks=[stdout_sink, ElasticSink()])
+    get_jobs_and_traces = GetJobsAndTraces(trace_size_limit=10000,
+                                           sinks=[stdout_sink, MongoDbSink(), ElasticSink()])
     repository_exporter = ExportRepository(repository_size_limit=1000000)
 
     gl = gitlab.Gitlab(url='https://gitlab.com', private_token=os.getenv("GITLAB_TOKEN"))
