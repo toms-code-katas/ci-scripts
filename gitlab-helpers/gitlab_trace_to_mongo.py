215fd98824346c2fa311863824edefa72748a81a
tom1299 <tom1299@jeeatwork.com>
2022-10-14 08:09:40 +0200

    Add debug print statements

--- gitlab-helpers/gitlab_trace_to_mongo.py
+++ gitlab-helpers/gitlab_trace_to_mongo.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     private_token = os.getenv("GITLAB_TOKEN", os.getenv("CI_JOB_TOKEN"))
     project_id = os.getenv("CI_PROJECT_ID")
     pipeline_id = os.getenv("CI_PIPELINE_ID")
-    trace_size_limit = os.getenv("TRACE_SIZE_LIMIT", "1000000")
+    trace_size_limit = int(os.getenv("TRACE_SIZE_LIMIT", "1000000"))
 
     gitlab = Gitlab(url=gitlab_url, private_token=private_token)
 
@@ -23,8 +23,10 @@ if __name__ == '__main__':
     pipeline = project.pipelines.get(id=pipeline_id)
     collected_jobs = []
 
+    print(f"Collecting jobs and trace logs for pipeline {pipeline.id} of project {project.id}")
     for pipeline_job in pipeline.jobs.list(get_all=True):
         complete_job = project.jobs.get(pipeline_job.id)
+        print(f"Found job {complete_job.id}")
         job_as_dict = complete_job.asdict()
 
         for artifact in complete_job.attributes["artifacts"]:
@@ -33,6 +35,7 @@ if __name__ == '__main__':
 
         collected_jobs.append(job_as_dict)
 
+    print(f"Found {len(collected_jobs)} jobs for pipeline {pipeline.id}")
     pipeline_as_dict = pipeline.asdict()
     pipeline_as_dict["jobs"] = collected_jobs
     mongo_db["pipelines"].insert_one(pipeline_as_dict)
