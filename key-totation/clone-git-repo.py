e1198b17232140eb5a47d56f8b816cd6a8ccb40e
tom1299 <tom1299@jeeatwork.com>
2022-12-23 08:23:26 +0100

    Add script for cloning including submodules

---
+++ key-totation/clone-git-repo.py
@@ -0,0 +1,46 @@
+import logging
+import sys
+import pexpect
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(message)s',
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
+
+logger = logging.getLogger(__name__)
+
+def send_authentication(expect_command, user_name, user_password):
+    expect_command.expect("Username for ")
+    expect_command.sendline(user_name)
+    expect_command.expect("Password for ")
+    expect_command.sendline(user_password)
+
+
+def clone_repo(repo_url, repo_name, user_name, user_password):
+    logger.info(f"\033[1;36mCloning {repo_url} into {repo_name} as {user_name}\033[0m")
+    clone_command = pexpect.spawn(f"git clone {repo_url} {repo_name}")
+    clone_command.logfile_read = sys.stdout.buffer
+    send_authentication(clone_command, user_name, user_password)
+    clone_command.expect(pexpect.EOF)
+    logger.info(f"\033[1;92m\U00002714 Successfully cloned {repo_name}\033[0m")
+
+
+def init_submodules(repo_name, user_name, user_password):
+    logger.info(f"\033[1;36mInitializing submodules in {repo_name}\033[0m")
+    init_command = pexpect.spawn(f"git submodule update --init --recursive", cwd=repo_name)
+    init_command.logfile_read = sys.stdout.buffer
+    send_authentication(init_command, user_name, user_password)
+    init_command.expect(pexpect.EOF)
+    logger.info(f"\033[1;92m\U00002714 Successfully initialized submodules\033[0m")
+
+
+if __name__ == '__main__':
+    repo_url = sys.argv[1]
+    repo_name = repo_url.split('/')[-1].split('.')[0]
+
+    user_name = sys.argv[2]
+    user_password = sys.argv[3]
+
+    clone_repo(repo_url, repo_name, user_name, user_password)
+    init_submodules(repo_name, user_name, user_password)
