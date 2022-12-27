441a8d507b11cc771ff2bfe8714fb4cc039ffd32
tom1299 <tom1299@jeeatwork.com>
2022-12-27 09:22:23 +0100

    Rename script and add all changes instead of one file only

--- key-totation/commit-and-push-all.py
+++ key-totation/commit-and-push-all.py
@@ -17,11 +17,10 @@ def send_authentication(expect_command, user_name, user_password):
     expect_command.sendline(user_password)
 
 if __name__ == '__main__':
-    file = sys.argv[1]
-    branch = sys.argv[2]
-    commit_message = sys.argv[3]
-    user_name = sys.argv[4]
-    user_password = sys.argv[5]
+    branch = sys.argv[1]
+    commit_message = sys.argv[2]
+    user_name = sys.argv[3]
+    user_password = sys.argv[4]
 
     logger.info(f"\033[1;36mCreating {branch}\033[0m")
     checkout_command = pexpect.spawn(f"git checkout -b {branch}")
@@ -30,21 +29,21 @@ if __name__ == '__main__':
     checkout_command.expect(pexpect.EOF)
     logger.info(f"\033[1;92m\U00002714 Successfully checked out {branch}\033[0m")
 
-    logger.info(f"\033[1;36mAdding {file}\033[0m")
-    add_command = pexpect.spawn(f"git add {file}")
+    logger.info("\033[1;36mAdding all changed and new files\033[0m")
+    add_command = pexpect.spawn("git add --all")
     add_command.logfile_read = sys.stdout.buffer
     add_command.expect(pexpect.EOF)
     add_command.close()
-    assert add_command.exitstatus == 0, f"Failed to add file {file}"
-    logger.info(f"\033[1;92m\U00002714 Successfully added {file}\033[0m")
+    assert add_command.exitstatus == 0, "Failed to add files"
+    logger.info("\033[1;92m\U00002714 Successfully added all files\033[0m")
 
-    logger.info(f"\033[1;36mCommitting {file}\033[0m")
+    logger.info("\033[1;36mCommitting changes\033[0m")
     commit_command = pexpect.spawn(f"git commit -m '{commit_message}'")
     commit_command.logfile_read = sys.stdout.buffer
     commit_command.expect(pexpect.EOF)
     add_command.close()
-    assert add_command.exitstatus == 0, f"Failed to commit file {file}"
-    logger.info(f"\033[1;92m\U00002714 Successfully committed {file}\033[0m")
+    assert add_command.exitstatus == 0, "Failed to commit changes"
+    logger.info(f"\033[1;92m\U00002714 Successfully committed all changes\033[0m")
 
     logger.info(f"\033[1;36mPushing {branch}\033[0m")
     push_command = pexpect.spawn(f"git push origin {branch}")
