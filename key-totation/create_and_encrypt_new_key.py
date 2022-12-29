aaf22720609cfc13e59437f70a8eb85f0ab36b2b
tom1299 <tom1299@jeeatwork.com>
2022-12-29 08:03:25 +0100

    Change path to cluster locations file to cwd

--- key-totation/create_and_encrypt_new_key.py
+++ key-totation/create_and_encrypt_new_key.py
@@ -30,35 +30,41 @@ def generate_new_key() -> str:
 
 
 def create_and_encrypt_new_key_file(key:str, cluster, password: str):
-    key_file_name = datetime.now().strftime(f"identity.agekey.%Y-%m-%d-%H-%M.{cluster}")
+    key_file_name = datetime.now().strftime(f"{os.getcwd()}/identity.agekey.%Y-%m-%d-%H-%M.{cluster}")
     with open(key_file_name, "w") as kf:
         kf.write(key)
     logger.info(f"\033[1;36mEncrypting key file {key_file_name}\033[0m")
-    age = pexpect.spawn(f"age -a -p {key_file_name}")
-    age.expect("Enter.*")
-    age.sendline(password)
-    age.expect("Confirm.*")
-    age.sendline(password)
-    age.expect("\r\n")
-    age.expect(pexpect.EOF)
+    encrypt_command = pexpect.spawn(f"age -a -p {key_file_name}")
+    encrypt_command.logfile_read = sys.stdout.buffer
+    encrypt_command.expect("Enter.*")
+    encrypt_command.sendline(password)
+    encrypt_command.expect("Confirm.*")
+    encrypt_command.sendline(password)
+    encrypt_command.expect("\r\n")
+    encrypt_command.expect(pexpect.EOF)
     with open(key_file_name + ".enc", "wb") as kf:
-        kf.write(age.before)
-    age.close()
-    assert age.exitstatus == 0, "Failed to encrypt key file"
+        kf.write(encrypt_command.before)
+    encrypt_command.close()
+    assert encrypt_command.exitstatus == 0, "Failed to encrypt key file"
     logger.info(f"\033[1;92m\U00002714 Successfully encrypted key file {key_file_name}\033[0m")
     os.remove(key_file_name)
 
 
-def get_secret_location(cluster: str) -> str:
-    with open(Path(__file__).with_name("secret-locations.yaml")) as f:
+def get_sops_config_file(secret_repo_path: str) -> str:
+    config_file = os.path.join(secret_repo_path, ".sops.yaml")
+    logger.info(f"\033[1;36mFound sops config file {config_file} for cluster {cluster}\033[0m")
+    return config_file
+
+def get_secret_location(cluster: str, secret_repo_path) -> str:
+    with open(os.path.join(os.getcwd(), "secret-locations.yaml")) as f:
         secret_locations = yaml.safe_load(f)
-    secret_location = secret_locations[cluster]
+    secret_location = os.path.join(secret_repo_path, secret_locations[cluster])
     logger.info(f"\033[1;36mFound secret location {secret_location} for cluster {cluster}\033[0m")
     return secret_location
 
-def generate_k8s_secret(cluster: str, key: str) -> str:
+def generate_k8s_secret(cluster: str, secret_repo_path: str, key: str) -> str:
     logger.info(f"\033[1;36mGenerating k8s secret for cluster {cluster}\033[0m")
-    k8s_secret_location = get_secret_location(cluster)
+    k8s_secret_location = get_secret_location(cluster, secret_repo_path)
     path = Path(k8s_secret_location).stem
     name, namespace = path.split("_")
     secret_as_dict = {
@@ -79,9 +85,9 @@ def generate_k8s_secret(cluster: str, key: str) -> str:
     return k8s_secret_location
 
 
-def encrypt_k8s_secret(k8s_secret_location: str):
+def encrypt_k8s_secret(k8s_secret_location: str, cluster: str, secret_repo_path: str):
     logger.info(f"\033[1;36mEncrypting k8s secret {k8s_secret_location}\033[0m")
-    encrypt_command = pexpect.spawn(f"sops --encrypt -i {k8s_secret_location}", cwd=Path(k8s_secret_location).parent)
+    encrypt_command = pexpect.spawn(f"sops --verbose --encrypt --config {get_sops_config_file(secret_repo_path)} -i {k8s_secret_location}")
     encrypt_command.logfile_read = sys.stdout.buffer
     encrypt_command.expect(pexpect.EOF)
     encrypt_command.close()
@@ -91,9 +97,10 @@ def encrypt_k8s_secret(k8s_secret_location: str):
 
 if __name__ == '__main__':
     cluster = sys.argv[1]
-    key_password = sys.argv[2]
+    secret_repo_path = sys.argv[2]
+    key_password = sys.argv[3]
 
     new_key = generate_new_key()
     create_and_encrypt_new_key_file(new_key, cluster, key_password)
-    k8s_secret_location = generate_k8s_secret(cluster, new_key)
-    encrypt_k8s_secret(k8s_secret_location)
+    k8s_secret_location = generate_k8s_secret(cluster, secret_repo_path, new_key)
+    encrypt_k8s_secret(k8s_secret_location, cluster, secret_repo_path)
